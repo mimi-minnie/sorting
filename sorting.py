7d277b68c4a3ba1aeb538a7a0c9917adcd5cc6ed
Mr. Swaggy-Muffin <glen1998@hotmail.com>
2014-05-16 14:12:36 +0100

    Update Code

--- sorting.py
+++ sorting.py
@@ -20,20 +20,20 @@ def bsort(alist):
                 swaps = True # Change swaps back to True
     return (alist) # returns the fully swapped alist
 
-def mini(alist):
-    answer = alist[0]
-    for item in alist:
-        if item< answer:
-            answer = item
-    return (answer)
+def mini(alist): # create a function: mini
+    answer = alist[0] # set the answer to the first value in alist
+    for item in alist: # for every item in alist...
+        if item< answer: # if the item is smaller than the answer...
+            answer = item # set the item as the new answer
+    return (answer) # return answer into the function
 
-def ssort(alist):
-    blist = []
-    while len(alist >0):
-        N = mini(alist)
-        alist.remove (N)
-        blist.append(N)
-    return (blist)
+def ssort(alist): # create a function: ssort
+    blist = [] # blist is set to nothing
+    while len(alist >0): # while the length of alist is bigger than 0
+        N = mini(alist) # set n as the mini function of alist
+        alist.remove (N) # remove this n from alist
+        blist.append(N) # add this n to blist
+    return (blist) # return blist into the function
 
     
 def mergeSort(alist)
@@ -44,7 +44,7 @@ def mergeSort(alist)
     This has some errors
     '''
     
-    if len(alist) >= 1:
+    if len(alist >= 1): # if the length of alist is greater than or equal to one...
         return (alist)
  
     mIndex = len(alist) \ 2
